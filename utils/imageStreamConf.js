5ceab2b47392e082fdeece13940775a94836bab6
franksportsapi <120779658+franksportsapi@users.noreply.github.com>
2025-04-03 18:48:44 +0530

    Update imageStreamConf.js

--- utils/imageStreamConf.js
+++ utils/imageStreamConf.js
@@ -1,13 +1,21 @@
-module.exports = Object.freeze({
+const CLOUDFLARE_PROVIDER = "cloudflare";
+
+const CLOUDFLARE_CONFIG = {
   enabled: false,
-  CLOUDFLARE: "cloudflare",
-  provider: "cloudflare",
-  cloudflare: {
+  provider: CLOUDFLARE_PROVIDER,
+  CLOUDFLARE: CLOUDFLARE_PROVIDER,
+  credentials: {
     account_id: "",
     account_hash: "",
-    token: "",
-    url: "https://api.cloudflare.com/client/v4/accounts/account_id/images/v1",
+    token: ""
+  },
+  endpoints: {
+    base_url(account_id) {
+      return `https://api.cloudflare.com/client/v4/accounts/${account_id}/images/v1`;
+    },
     upload_image: "uploadImage",
     remove_image: "removeImage"
   }
-});
+};
+
+module.exports = Object.freeze(CLOUDFLARE_CONFIG);
