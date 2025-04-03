cf4aa996f7aad6f2d9b38d49be68312473c5b82d
franksportsapi <120779658+franksportsapi@users.noreply.github.com>
2025-04-03 18:19:28 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
-module.exports = {
-  API_INITIAL_ROUTE_V1: "/api/v1",
-  ADMIN_PORT_1: 4000,
-  USER_PORT_1: 4050
-}
+const CONFIG = {
+  API: {
+    INITIAL_ROUTE_V1: "/api/v1"
+  },
+  PORTS: {
+    ADMIN: 4000,
+    USER: 4050
+  }
+};
+
+module.exports = CONFIG;
